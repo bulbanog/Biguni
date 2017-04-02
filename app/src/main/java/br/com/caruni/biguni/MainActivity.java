013bc5fc3d3abdc4ea013f6eb41b34bdbc0c958b
TOSHIBA\JOHN <johnoestoy@gmail.com>
2017-04-02 20:48:54 -0300

    Tentativa #1000

--- app/src/main/java/br/com/caruni/biguni/MainActivity.java
+++ app/src/main/java/br/com/caruni/biguni/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.caruni.biguni;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity","onStart");
     }
-}
+    @Override
+    protected void onStart(){
+        super.onCreate();
+        Log.i("MainActivity","onStart");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "onStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "onDestroy");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MainActivity", "onRestart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity", "onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
--- app/src/main/java/br/com/caruni/biguni/SecondActivity.java
+++ app/src/main/java/br/com/caruni/biguni/SecondActivity.java
@@ -1,10 +1,49 @@
 package br.com.caruni.biguni;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by JOHN on 2/04/2017.
  */
 
 public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreat(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("SecondActivity","onCreat");
+    }
+    @Override
+    protected void onStart(){
+        super.onCreate();
+        Log.i("SecondActivity","onStart");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("SecondActivity", "onStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("SecondActivity", "onDestroy");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("SecondActivity", "onRestart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("SecondActivity", "onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("SecondActivity", "onPause");
+    }
 }
