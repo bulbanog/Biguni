117ef8fca2fedd4d6aa1d36ccef0b3f2531b1e4e
TOSHIBA\JOHN <johnoestoy@gmail.com>
2017-04-02 18:29:28 -0300

    Primeiro commit (Espero funcione bem)

---
+++ app/src/main/java/br/com/caruni/biguni/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.caruni.biguni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
