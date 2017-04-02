f9e1e5a682e1f753960dff24c364bcc4db00e56d
TOSHIBA\JOHN <johnoestoy@gmail.com>
2017-04-02 18:49:21 -0300

    Segunda Activity

---
+++ app/src/main/java/br/com/caruni/biguni/SecondActivity.java
@@ -0,0 +1,10 @@
+package br.com.caruni.biguni;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JOHN on 2/04/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+}
